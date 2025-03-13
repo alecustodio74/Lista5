3c1d15d6a4cf7ea610bc042dc47937f4d252ac21
Alexandre Custódio <arcs1234@hotmail.com>
2025-03-13 20:58:54 -0300

    Create 1_contatos.php

---
+++ 1_contatos.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt_BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Alexandre Ricardo Custódio de Souza">
+    <title>1 - Lista de Contatos</title>
+</head>
+<body>
+    <form action="" method="POST">
+        <?php
+        for($i=0;$i<5;$i++): ?>
+            <input type="text" name="nome[]" placeholder="Nome" />
+            <input type="number" name="tel[]" placeholder="Telefone"/>
+        </br>
+        <?php endfor; ?>
+        <button type="submit">Enviar</button>
+</form>
+
+<?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+        try{
+            $a = array();
+            $nome = $_POST['nome'];
+            $tel = $_POST['tel'];
+            for($i=0;$i<5;$i++){
+                $posicao = $nome[$i];
+                $a[$posicao] = $tel[$i];
+            }
+        }catch (Exception $e){
+            echo $e->getMessage();
+    }
+    var_dump($a);
+}
+?>
+    
+</body>
+</html>
